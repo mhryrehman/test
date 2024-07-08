081e02f81c8cb02e7639c41f4096c656209376e0
Yasir Rehman <yasir.rehman@curemd.com>
2024-07-08 14:29:47 +0500

    my commit

---
+++ src/main/java/com/example/keycloakadapterdemo/KeyCloakAdaptorDemoConstants.java
@@ -0,0 +1,7 @@
+package com.example.keycloakadapterdemo;
+
+public class KeyCloakAdaptorDemoConstants {
+public static String name="yasir";
+public static String age="25";
+public static String gender="male";
+}
